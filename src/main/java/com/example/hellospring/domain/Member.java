34bb1c77d5d72f4f1c2bad45ceb003308bbeeab2
hiwon-lee <leather2790@naver.com>
2023-11-06 11:05:48 +0900

    [fix] javax to jakarta

--- src/main/java/com/example/hellospring/domain/Member.java
+++ src/main/java/com/example/hellospring/domain/Member.java
@@ -1,14 +1,17 @@
 package com.example.hellospring.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
+
 
 @Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     public Long getId() {
