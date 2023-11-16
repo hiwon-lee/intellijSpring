eae363a44bfdc75393adbb94bdfac2419236ea34
hiwon-lee <leather2790@naver.com>
2023-11-16 23:50:54 +0900

    [feat] add lombok

--- src/main/java/com/example/hellospring/config/WebSocketConfig.java
+++ src/main/java/com/example/hellospring/config/WebSocketConfig.java
@@ -1,23 +1,27 @@
 package com.example.hellospring.config;
 
+import lombok.RequiredArgsConstructor;
+
 import com.example.hellospring.controller.MyWebSocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-@Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(myHandler(), "/my-websocket-endpoint");
-    }
+import java.security.Security;
 
-    @Bean
-    public WebSocketHandler myHandler() {
-        return new MyWebSocketHandler();
-    }
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketConfigurer {
+   @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       http.authorizeHttpRequests().anyRequest("/").permitAll();
+
+   }
 }
